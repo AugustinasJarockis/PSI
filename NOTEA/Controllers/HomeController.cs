4db8ade71af012283d2417988140058e3636a3cf
GitaGliaudyte <gita.gliaudyte@gmail.com>
2023-09-18 16:52:15 +0300

    Adding something.

--- NOTEA/Controllers/HomeController.cs
+++ NOTEA/Controllers/HomeController.cs
@@ -23,6 +23,18 @@ namespace NOTEA.Controllers
             return View();
         }
 
+        public IActionResult GetStarted()
+        {
+            return View();
+        }
+
+        public IActionResult HowToUse()
+        {
+            return View();
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
