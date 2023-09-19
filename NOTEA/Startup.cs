477db4c0558fba64adb91c5ad938eed6823957b5
Domarkaite <119532702+Domarkaite@users.noreply.github.com>
2023-09-19 18:54:22 +0300

    Adding conspects

---
+++ NOTEA/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(NOTEA.Startup))]
+namespace NOTEA
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var serviceCollection = builder.Services;
+            var configuration = builder.GetContext().Configuration;
+
+            //builder.Services.AddSingleton<>();
+        }
+    }
+}
