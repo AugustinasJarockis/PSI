7ca9f7eae3b335778ab978227ed867709c88ee38
“Augustinas” <jarauga@gmail.com>
2023-10-04 13:50:33 +0300

    Added extension method to string

---
+++ NOTEA/Models/StringValueComparer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace NOTEA.Models
+{
+    public static class StringValueComparer
+    {
+        public static bool IsGreaterValue(this string str, string other_str)
+        {
+            if (str.Length > other_str.Length)
+                return true;
+            if (str.Length < other_str.Length)
+                return false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] > other_str[i])
+                    return true;
+                if (str[i] < other_str[i])
+                    return false;
+            }
+            return false;
+        }
+    }
+}
