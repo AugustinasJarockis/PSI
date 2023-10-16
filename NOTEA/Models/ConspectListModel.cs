30786718b36a714bfd3bf2b2eda2af3f5574cba6
GitaGliaudyte <gita.gliaudyte@gmail.com>
2023-10-16 12:49:22 +0300

    adding optional arguments for linq

--- NOTEA/Models/ConspectListModel.cs
+++ NOTEA/Models/ConspectListModel.cs
@@ -2,11 +2,12 @@
 
 namespace NOTEA.Models
 {
-    public class ConspectListModel<ConspectType>
+    public class ConspectListModel<ConspectType, ConspectSemesterType>
     {
-        public List<ConspectType> conspects = new List<ConspectType>();
+        public List<ConspectType> Conspects = new List<ConspectType>();
+        public List<ConspectSemesterType> ConspectSemesters = new List<ConspectSemesterType>();
         public ConspectListModel() { }
-        public ConspectListModel(List<ConspectType> conspects) { this.conspects = conspects; }
+        public ConspectListModel(List<ConspectType> conspects, List<ConspectSemesterType> semesters) { this.ConspectSemesters = semesters; this.Conspects = conspects; }
 
     }
 
