6c1da569d1b62f3d70b6549a8646a79b83a5c5d2
RutaKisieliute <118683107+RutaKisieliute@users.noreply.github.com>
2023-10-16 23:01:03 +0300

    Navbar fix and addition of user servises

---
+++ NOTEA/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using NOTEA.Models;
+
+namespace NOTEA.Services
+{
+    public interface IUserService
+    {
+        public void SaveUsers(UserListModel userList);
+        public UserListModel LoadUsers();
+    }
+}
--- NOTEA/Services/UserService.cs
+++ NOTEA/Services/UserService.cs
@@ -1,6 +1,28 @@
-﻿namespace NOTEA.Services
+﻿using Newtonsoft.Json;
+using NOTEA.Models;
+
+namespace NOTEA.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public UserListModel LoadUsers()
+        {
+            string text = File.ReadAllText("Users//Users.txt");
+            UserListModel userList = JsonConvert.DeserializeObject<UserListModel>(text);
+            return userList;
+        }
+        public void SaveUsers(UserListModel userList)
+        {
+            using (StreamWriter writer = new StreamWriter("Users//Users.txt"))
+                try
+                {
+                    string serializedJSON = JsonConvert.SerializeObject(userList);
+                    writer.Write(serializedJSON);
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine("Error: could not save user ");
+                }
+        }
     }
 }
