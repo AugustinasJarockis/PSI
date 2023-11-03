73ac30959df93e6ad9e3e1c14bacdb360598684e
Domarkaite <119532702+Domarkaite@users.noreply.github.com>
2023-11-04 00:05:29 +0200

    Adding something that im not sure of

---
+++ NOTEA/Models/DatabaseMano.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NOTEA.Models.ConspectModels;
+using NOTEA.Models.UserModels;
+
+namespace NOTEA.Models
+{
+    public class DatabaseMano : DbContext
+    {
+        public DbSet<ConspectModel> Conspects { get; set; }
+
+        //public DbSet<UserModel> Users { get; set; }     
+
+        public DatabaseMano(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
