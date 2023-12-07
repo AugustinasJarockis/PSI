8d832a6dbd3299fec6468713ece1af3979012001
RutaKisieliute <118683107+RutaKisieliute@users.noreply.github.com>
2023-12-07 18:42:15 +0200

    Fixing view

--- NOTEA/Migrations/20231205122559_a.Designer.cs
+++ NOTEA/Migrations/20231205122559_a.Designer.cs
@@ -12,8 +12,8 @@ using NOTEA.Database;
 namespace NOTEA.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    [Migration("20231203205951_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20231205122559_a")]
+    partial class a
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
